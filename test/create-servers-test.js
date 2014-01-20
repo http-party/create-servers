f5d99dbd9e9fc6c00ac99576b47957c38db1f8fe
indexzero <charlie.robbins@gmail.com>
2014-01-19 22:52:28 -0500

    [test] Added tests

---
+++ test/create-servers-test.js
@@ -0,0 +1,73 @@
+/*
+ * create-servers-test.js: Make sure creating both works
+ *
+ * (C) 2013, Charlie Robbins.
+ *
+ */
+
+var path = require('path'),
+    http = require('http'),
+    test = require('tape'),
+    createServers = require('../');
+
+//
+// Immediately end a response.
+//
+function fend(req, res) {
+  res.end();
+}
+
+test('only http', function (t) {
+  t.plan(3);
+  createServers({
+    log: console.log,
+    http: 9876
+  }, fend, function (err, servers) {
+    console.dir(err);
+    t.error(err);
+    t.equals(typeof servers, 'object');
+    t.equals(typeof servers.http, 'object');
+    servers.http.close();
+  });
+});
+
+test('only https', function (t) {
+  t.plan(3);
+  createServers({
+    log: console.log,
+    https: {
+      port: 3456,
+      root: path.join(__dirname, 'fixtures'),
+      cert: 'agent2-cert.pem',
+      key:  'agent2-key.pem'
+    }
+  }, fend, function (err, servers) {
+    console.dir(err);
+    t.error(err);
+    t.equals(typeof servers, 'object');
+    t.equals(typeof servers.https, 'object');
+    servers.https.close();
+  });
+});
+
+test('http && https', function (t) {
+  t.plan(4);
+  createServers({
+    log: console.log,
+    http: 8765,
+    https: {
+      port: 3456,
+      root: path.join(__dirname, 'fixtures'),
+      cert: 'agent2-cert.pem',
+      key:  'agent2-key.pem'
+    }
+  }, fend, function (err, servers) {
+    console.dir(err);
+    t.error(err);
+    t.equals(typeof servers, 'object');
+    t.equals(typeof servers.http, 'object');
+    t.equals(typeof servers.https, 'object');
+    servers.http.close();
+    servers.https.close();
+  });
+});
